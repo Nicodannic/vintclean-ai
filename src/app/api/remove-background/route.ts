1fbc12d7ac55c34726a17b427709e335ebaffb4b
Nicodannic <lesjeuxvideodannic@gmail.com>
2026-07-26 00:09:38 +0200

    connect render ai api

--- src/app/api/remove-background/route.ts
+++ src/app/api/remove-background/route.ts
@@ -1,4 +1,10 @@
 import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
 
 export async function POST(req: Request) {
   try {
@@ -6,25 +12,73 @@ export async function POST(req: Request) {
 
     if (!imageUrl) {
       return NextResponse.json(
-        {
-          error: "Aucune image reçue"
-        },
-        {
-          status: 400
-        }
+        { error: "Image manquante" },
+        { status: 400 }
       );
     }
 
-    console.log("Image reçue :", imageUrl);
+    // Télécharger l'image originale
+    const imageResponse = await fetch(imageUrl);
+
+    if (!imageResponse.ok) {
+      throw new Error("Impossible de récupérer l'image");
+    }
+
+    const imageBuffer = await imageResponse.arrayBuffer();
+
+    // Envoyer l'image à notre IA Render
+    const formData = new FormData();
+
+    formData.append(
+      "file",
+      new Blob([imageBuffer]),
+      "image.webp"
+    );
+
+    const aiResponse = await fetch(
+      "https://vintclean-ai-api.onrender.com/remove-background",
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+    if (!aiResponse.ok) {
+      throw new Error("Erreur IA");
+    }
+
+    const processedBuffer = await aiResponse.arrayBuffer();
+
+    // Upload résultat dans Supabase
+    const fileName = `clean-${Date.now()}.png`;
+
+    const { error } = await supabaseAdmin.storage
+      .from("processed-images")
+      .upload(
+        fileName,
+        processedBuffer,
+        {
+          contentType: "image/png",
+        }
+      );
+
+    if (error) {
+      throw error;
+    }
+
+    const { data } = supabaseAdmin.storage
+      .from("processed-images")
+      .getPublicUrl(fileName);
+
 
     return NextResponse.json({
-      message: "Image reçue, IA prête à être connectée 🚀",
-      imageUrl: imageUrl
+      success: true,
+      processedImage: data.publicUrl,
     });
 
   } catch (error) {
 
-    console.log("Erreur :", error);
+    console.error(error);
 
     return NextResponse.json(
       {
